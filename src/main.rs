1c955a76cc6fc6666b4758eb58a86fd9ef977d0f
Markus Petzke <markuspetzke@gmail.com>
2025-11-02 23:21:04 +0100

    dynamic rendering list

--- src/main.rs
+++ src/main.rs
@@ -67,95 +67,127 @@ fn window() {
         program
     };
 
-    let mut square = SquareObject::new(
-        Vec3::new(400.0, 300.0, 0.0),
+    let square = SquareObject::new(
+        Vec3::new(200.0, 300.0, 0.0),
         100.0,
         Vec3::new(0.5, 0.5, 0.2),
     );
 
-    let mut ball = BallObject::new(
+    let ball1 = BallObject::new(
         Vec3::new(400.0, 300.0, 0.0),
         Vec2::new(150.0, 200.0),
-        100.0,
+        10.0,
         Vec3::new(0.5, 0.5, 0.2),
     );
+
+    let ball2 = BallObject::new(
+        Vec3::new(500.0, 200.0, 0.0),
+        Vec2::new(150.0, 200.0),
+        10.0,
+        Vec3::new(0.5, 0.5, 0.2),
+    );
+
+    let mut ball_objects = vec![ball1, ball2];
+    let mut square_objects = vec![square];
+
     let mut last_time = glfw.get_time() as f32;
-    let speed = 20000.0;
 
     // Render loop
     while !window.should_close() {
         let current_time = glfw.get_time() as f32;
         let delta_time = current_time - last_time;
 
+        for ball in &mut ball_objects {
+            ball.update(delta_time);
+
+            let wall_collision = check_wall_collision(
+                ball.position,
+                ball.radius,
+                SRC_WIDTH as f32,
+                SRC_HEIGHT as f32,
+            );
+
+            if wall_collision.left || wall_collision.right {
+                ball.velocity.x *= -1.0;
+                if wall_collision.left {
+                    ball.position.x = ball.radius;
+                }
+                if wall_collision.right {
+                    ball.position.x = SRC_WIDTH as f32 - ball.radius;
+                }
+            }
+
+            if wall_collision.top || wall_collision.bottom {
+                ball.velocity.y *= -1.0;
+                if wall_collision.bottom {
+                    ball.position.y = ball.radius;
+                }
+                if wall_collision.top {
+                    ball.position.y = SRC_HEIGHT as f32 - ball.radius;
+                }
+            }
+        }
+
+        for ball in &mut ball_objects {
+            let mut collision_detected = false;
+
+            for square in &square_objects {
+                if check_ball_square_collision(
+                    ball.position,
+                    ball.radius,
+                    square.position,
+                    square.size,
+                ) {
+                    ball.velocity *= -1.0;
+                    collision_detected = true;
+                    break;
+                }
+            }
+        }
+
+        for square in &mut square_objects {
+            let mut collision = false;
+            for ball in &ball_objects {
+                if check_ball_square_collision(
+                    ball.position,
+                    ball.radius,
+                    square.position,
+                    square.size,
+                ) {
+                    collision = true;
+                    break;
+                }
+            }
+
+            if collision {
+                square.color = Vec3::new(1.0, 0.5, 0.0);
+            } else {
+                square.color = Vec3::new(0.3, 0.8, 1.0);
+            }
+        }
+
         unsafe {
             gl::ClearColor(0.2, 0.3, 0.3, 1.0);
             gl::Clear(gl::COLOR_BUFFER_BIT);
 
-            // square.rotation = glfw.get_time() as f32;
-
             let ortho =
                 Mat4::orthographic_rh_gl(0.0, SRC_WIDTH as f32, 0.0, SRC_HEIGHT as f32, -1.0, 1.0);
-            square.render(shader_program, &ortho);
-            ball.render(shader_program, &ortho);
+            for ball in &ball_objects {
+                ball.render(shader_program, &ortho);
+            }
+
+            for square in &square_objects {
+                square.render(shader_program, &ortho);
+            }
         }
 
         glfw.poll_events();
         for (_, event) in glfw::flush_messages(&events) {
-            match event {
-                glfw::WindowEvent::Key(Key::Escape, _, Action::Press, _) => {
-                    window.set_should_close(true);
-                }
-                glfw::WindowEvent::Key(Key::D, _, Action::Press, _) => {
-                    ball.position.x += speed * delta_time;
-                }
-                glfw::WindowEvent::Key(Key::A, _, Action::Press, _) => {
-                    ball.position.x -= speed * delta_time;
-                }
-                glfw::WindowEvent::Key(Key::W, _, Action::Press, _) => {
-                    ball.position.y += speed * delta_time;
-                }
-                glfw::WindowEvent::Key(Key::S, _, Action::Press, _) => {
-                    ball.position.y -= speed * delta_time;
-                }
-                _ => {}
+            if let glfw::WindowEvent::Key(Key::Escape, _, Action::Press, _) = event {
+                window.set_should_close(true);
             }
         }
 
-        ball.update(delta_time);
-
-        let wall_collision = check_wall_collision(
-            ball.position,
-            ball.radius,
-            SRC_WIDTH as f32,
-            SRC_HEIGHT as f32,
-        );
-        if wall_collision.left || wall_collision.right {
-            ball.velocity.x *= -1.0;
-            if wall_collision.left {
-                ball.position.x = ball.radius;
-            }
-            if wall_collision.right {
-                ball.position.x = SRC_WIDTH as f32 - ball.radius;
-            }
-        }
-
-        if wall_collision.top || wall_collision.bottom {
-            ball.velocity.y *= -1.0;
-            if wall_collision.bottom {
-                ball.position.y = ball.radius;
-            }
-            if wall_collision.top {
-                ball.position.y = SRC_HEIGHT as f32 - ball.radius;
-            }
-        }
-
-        if check_ball_square_collision(ball.position, ball.radius, square.position, square.size) {
-            ball.color = Vec3::new(1.0, 1.0, 0.0);
-            square.color = Vec3::new(1.0, 0.5, 0.0);
-        } else {
-            ball.color = Vec3::new(1.0, 0.3, 0.3);
-            square.color = Vec3::new(0.3, 0.8, 1.);
-        }
         last_time = current_time;
         window.swap_buffers();
     }
